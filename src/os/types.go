2e0fb371ae4b0a5e4498a2ab9685149dbfce4ce1
Michał Derkacz <michal@Lnet.pl>
2020-12-12 19:15:28 +0100

    os: backport type FileInfo = fs.FileInfo

--- src/os/types.go
+++ src/os/types.go
@@ -7,7 +7,6 @@ package os
 import (
 	"io/fs"
 	"syscall"
-	"time"
 )
 
 // Getpagesize returns the underlying system's memory page size.
@@ -19,14 +18,7 @@ type File struct {
 }
 
 // A FileInfo describes a file and is returned by Stat and Lstat.
-type FileInfo interface {
-	Name() string       // base name of the file
-	Size() int64        // length in bytes for regular files; system-dependent for others
-	Mode() FileMode     // file mode bits
-	ModTime() time.Time // modification time
-	IsDir() bool        // abbreviation for Mode().IsDir()
-	Sys() interface{}   // underlying data source (can return nil)
-}
+type FileInfo = fs.FileInfo
 
 // A FileMode represents a file's mode and permission bits.
 // The bits have the same definition on all systems, so that
