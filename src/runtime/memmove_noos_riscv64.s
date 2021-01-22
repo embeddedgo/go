e255be68aa15ed741d156032929dea6dbbb81ccb
Michał Derkacz <michal@Lnet.pl>
2021-01-22 18:37:30 +0100

    runtime: fix memmove on noos/riscv64

--- src/runtime/memmove_noos_riscv64.s
+++ src/runtime/memmove_noos_riscv64.s
@@ -8,22 +8,96 @@
 
 // void runtime·memmove(void*, void*, uintptr)
 TEXT runtime·memmove(SB),NOSPLIT,$-0-24
+	MOV  n+16(FP), A2
+	BEQ  A2, ZERO, done
 	MOV  to+0(FP), A0
 	MOV  from+8(FP), A1
-	MOV  n+16(FP), A2
+
+	// check alignment
+	// The code below requires all of the "to", "from", "n" are aligned
+	// to use multi-byte load/stores. TODO: relax this requirement.
+	OR   A0, A1, S0
+	OR   A2, S0
+	AND  $7, S0
+	AND  $3, S0, S1
+	AND  $1, S0, S2
+
+	// check the relative position of the "to" and "from"
+	BLTU  A1, A0, backward
+
 	ADD  A0, A2
+	BEQ  S0, ZERO, forwardLoop8
+	BEQ  S1, ZERO, forwardLoop4
+	BEQ  S2, ZERO, forwardLoop2
 
-	// In case od noos we can't use unaligned load/stores.
-	// For now we just copy bytes.
-	// TODO: optimize this
-
-loop:
-	BEQ    A0, A2, done
+forwardLoop1:
 	MOVBU  (A1), S0
 	ADD    $1, A1
 	MOVB   S0, (A0)
 	ADD    $1, A0
-	JMP    loop
-
+	BNE    A0, A2, forwardLoop1
 done:
+	RET
+
+forwardLoop8:
+	MOV  (A1), S0
+	ADD  $8, A1
+	MOV  S0, (A0)
+	ADD  $8, A0
+	BNE  A0, A2, forwardLoop8
+	RET
+
+forwardLoop4:
+	MOVWU  (A1), S0
+	ADD    $4, A1
+	MOVW   S0, (A0)
+	ADD    $4, A0
+	BNE    A0, A2, forwardLoop4
+	RET
+
+forwardLoop2:
+	MOVHU  (A1), S0
+	ADD    $2, A1
+	MOVH   S0, (A0)
+	ADD    $2, A0
+	BNE    A0, A2, forwardLoop2
+	RET
+
+backward:
+	ADD  A2, A1
+	ADD  A0, A2
+	BEQ  S0, ZERO, backwardLoop8
+	BEQ  S1, ZERO, backwardLoop4
+	BEQ  S2, ZERO, backwardLoop2
+
+backwardLoop1:
+	ADD    $-1, A1
+	MOVBU  (A1), S0
+	ADD    $-1, A2
+	MOVB   S0, (A2)
+	BNE    A0, A2, backwardLoop1
+	RET
+
+backwardLoop8:
+	ADD  $-8, A1
+	MOV  (A1), S0
+	ADD  $-8, A2
+	MOV  S0, (A2)
+	BNE  A0, A2, backwardLoop8
+	RET
+
+backwardLoop4:
+	ADD    $-4, A1
+	MOVWU  (A1), S0
+	ADD    $-4, A2
+	MOVW   S0, (A2)
+	BNE    A0, A2, backwardLoop4
+	RET
+
+backwardLoop2:
+	ADD    $-2, A1
+	MOVHU  (A1), S0
+	ADD    $-2, A2
+	MOVH   S0, (A2)
+	BNE    A0, A2, backwardLoop2
 	RET
