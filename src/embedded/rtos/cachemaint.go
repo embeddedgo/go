46ee28d7998fa8b5cc05e404addff672de03744a
Michał Derkacz <michal@Lnet.pl>
2022-08-12 01:12:33 +0200

    rtos: standarize 3 most common cache maintenance operations

--- src/embedded/rtos/cachemaint.go
+++ src/embedded/rtos/cachemaint.go
@@ -6,17 +6,14 @@ package rtos
 
 import "unsafe"
 
-// CacheMaint performs a cache maintenance operations specified by the
-// architecture dependent parameter op. The operation may affect an area of
-// memory larger than that specified by the p and size parameters.
 
-// CacheMaint op for ARMv7-M
 const (
-	ARMv7M_DCacheInvalPoC      = 0
-	ARMv7M_DCacheCleanPoC      = 1
-	ARMv7M_DCacheCleanInvalPoC = 2
+	DCacheInval      = 0
+	DCacheClean      = 1
+	DCacheCleanInval = 2
 )
 
+// CacheMaint performs a cache maintenance operations specified by op.
 func CacheMaint(op int, p unsafe.Pointer, size int) {
 	cacheMaint(op, p, size)
 }
