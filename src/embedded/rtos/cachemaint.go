1d3d3cefbf7efad55779e67b3ce16b4d28634975
Michał Derkacz <michal@Lnet.pl>
2022-08-11 18:18:26 +0200

    rtos: constants for supported ARMv7-M cache maintenance operations

--- src/embedded/rtos/cachemaint.go
+++ src/embedded/rtos/cachemaint.go
@@ -9,14 +9,14 @@ import "unsafe"
 // CacheMaint performs a cache maintenance operations specified by the
 // architecture dependent parameter op. The operation may affect an area of
 // memory larger than that specified by the p and size parameters.
-//
-// The meaning of "op" for different architectures.
-//
-// thumb:
-//  0. data cache invalidate to the point of coherency
-//  1. data cache clean to the point of coherency
-//  2. data cache clean and invalidate to the point of coherency
-//
+
+// CacheMaint op for ARMv7-M
+const (
+	ARMv7M_DCacheInvalPoC      = 0
+	ARMv7M_DCacheCleanPoC      = 1
+	ARMv7M_DCacheCleanInvalPoC = 2
+)
+
 func CacheMaint(op int, p unsafe.Pointer, size int) {
 	cacheMaint(op, p, size)
 }
