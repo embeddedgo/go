354718dec7bbe4eeb6c5cda4f038f3f1a81376f1
Michał Derkacz <michal@Lnet.pl>
2024-03-07 17:24:02 +0100

    embedded/rtos: better description of CacheMaint function

--- src/embedded/rtos/cachemaint.go
+++ src/embedded/rtos/cachemaint.go
@@ -15,7 +15,12 @@ const (
 	DCacheCleanInval = DCacheFlushInval // deprecated
 )
 
-// CacheMaint performs a cache maintenance operations specified by op.
+// CacheMaint performs a cache maintenance operations specified by op. This
+// operation applies to all cache lines that contain data from the memory
+// described by p and size. For this reason, the adjacent memory that shares
+// the same cache lines will be affected too. You must take this into account
+// if there are other bus masters (e.g. DMA) in the system that may use this
+// adjacent memory.
 func CacheMaint(op int, p unsafe.Pointer, size int) {
 	cacheMaint(op, p, size)
 }
