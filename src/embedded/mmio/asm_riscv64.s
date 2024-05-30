93d38c73a2c6918e78910ea475768edb31ab4cc1
Michał Derkacz <michal@Lnet.pl>
2024-05-31 01:55:33 +0200

    mmio: fix argument sizes in amd64 and riscv64 assembly

--- src/embedded/mmio/asm_riscv64.s
+++ src/embedded/mmio/asm_riscv64.s
@@ -1,49 +1,49 @@
 #include "textflag.h"
 
 
-TEXT ·load64(SB),NOSPLIT,$0-8
+TEXT ·load64(SB),NOSPLIT,$0-16
 	MOV  addr+0(FP), A0
 	MOV  (A0), A1
 	MOV  A1, ret+8(FP)
 	RET
 
-TEXT ·load32(SB),NOSPLIT,$0-8
+TEXT ·load32(SB),NOSPLIT,$0-12
 	MOV    addr+0(FP), A0
 	MOVWU  (A0), A1
 	MOVW   A1, ret+8(FP)
 	RET
 
-TEXT ·load16(SB),NOSPLIT,$0-6
+TEXT ·load16(SB),NOSPLIT,$0-10
 	MOVW   addr+0(FP), A0
 	MOVHU  (A0), A1
 	MOVH   A1, ret+8(FP)
 	RET
 
-TEXT ·load8(SB),NOSPLIT,$0-5
+TEXT ·load8(SB),NOSPLIT,$0-9
 	MOV    addr+0(FP), A0
 	MOVBU  (A0), A1
 	MOVB   A1, ret+8(FP)
 	RET
 
-TEXT ·store64(SB),NOSPLIT,$0-8
+TEXT ·store64(SB),NOSPLIT,$0-16
 	MOV  addr+0(FP), A0
 	MOV  v+8(FP), A1
 	MOV  A1, (A0)
 	RET
 
-TEXT ·store32(SB),NOSPLIT,$0-8
+TEXT ·store32(SB),NOSPLIT,$0-12
 	MOV    addr+0(FP), A0
 	MOVWU  v+8(FP), A1
 	MOVW   A1, (A0)
 	RET
 
-TEXT ·store16(SB),NOSPLIT,$0-6
+TEXT ·store16(SB),NOSPLIT,$0-10
 	MOV    addr+0(FP), A0
 	MOVHU  v+8(FP), A1
 	MOVH   A1, (A0)
 	RET
 
-TEXT ·store8(SB),NOSPLIT,$0-5
+TEXT ·store8(SB),NOSPLIT,$0-9
 	MOV    addr+0(FP), A0
 	MOVBU  v+8(FP), A1
 	MOVB   A1, (A0)
