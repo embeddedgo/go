298a74f91198f649ddce8d45e84f4c47ea27e671
Timur Celik <mail@timurcelik.de>
2024-06-11 15:33:09 +0200

    Revert "Add makeshift USB logging"
    This reverts commit a292acb81065410b72259cf5e9c21ca12370df13.

--- src/runtime/tasker_noos_mips64.go
+++ src/runtime/tasker_noos_mips64.go
@@ -128,64 +128,24 @@ func restoreGPRs()
 func saveFPRs()
 func restoreFPRs()
 
-var usbBuffer [512 + 16]byte // pad by 16 byte for aligment and cache ops
+var lastlog [1024 * 16]byte
+var lastidx = 0
 
-//go:nosplit
-func printFatal(cause uint, epc uint, status uint, badvaddr uint, ra uint) {
-	// don't use print() here since it will cause a syscall exception
-	defaultWrite(0, []byte("--- FATAL exception ---\n"))
-	defaultWrite(0, []byte("cause:    0x"))
-	printUint32Hex(cause)
-	defaultWrite(0, []byte("\nepc:      0x"))
-	printUint32Hex(epc)
-	defaultWrite(0, []byte("\nstatus:   0x"))
-	printUint32Hex(status)
-	defaultWrite(0, []byte("\nbadvaddr: 0x"))
-	printUint32Hex(badvaddr)
-	defaultWrite(0, []byte("\nra:       0x"))
-	printUint32Hex(ra)
-	defaultWrite(0, []byte("\n--------- END ---------\n"))
-}
-
-// Prints a uint32 in hex format.
-//
-//go:nosplit
-func printUint32Hex(n uint) {
-	var buf [8]byte
-	for i := range buf {
-		ascii := byte(n>>((7-i)*4)) & 0xf
-		ascii += 48
-		if ascii > 57 {
-			ascii += 39
-		}
-		buf[i] = ascii
-	}
-	defaultWrite(0, buf[:])
-}
-
-// Sends text messages over USB compatible with UNFLoader.
+// Just write into some ringbuffer for now, which can be inspected in a memory
+// dump.
 //
 //go:nowritebarrierrec
 //go:nosplit
 func defaultWrite(fd int, p []byte) int {
-	alignedStart := 16 - int(uintptr(unsafe.Pointer(&usbBuffer))%16)
-	alignedEnd := alignedStart + len(p) + 12
-	if alignedEnd%2 == 1 {
-		alignedEnd += 1
+	for i := 0; i < len(p); i++ {
+		lastlog[lastidx] = p[i]
+		lastidx += 1
+		if lastidx >= len(lastlog) {
+			lastidx = 0
+		}
 	}
-	usbBufferAligned := usbBuffer[alignedStart:alignedEnd]
-
-	copy(usbBufferAligned, "DMA@")
-	copy(usbBufferAligned[4:], []byte{1, 0, 0, byte(len(p))})
-	copy(usbBufferAligned[8:], p)
-	copy(usbBufferAligned[8+len(p):], "CMPH")
-	ptr := unsafe.SliceData(usbBufferAligned)
-	writeEverdrive64USB(unsafe.Pointer(ptr), len(usbBufferAligned))
 	return len(p)
 }
-
-func initEverdrive64USB()
-func writeEverdrive64USB(p unsafe.Pointer, size int)
 
 // syscalls not used by runtime
 func syssetprivlevel(newlevel int) (oldlevel, errno int)       { return } // TODO
