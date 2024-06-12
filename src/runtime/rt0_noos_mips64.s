90d1c561ebb0ad9e31992b663e5658ebb4f12517
Timur Celik <mail@timurcelik.de>
2024-06-13 01:59:07 +0200

    Use Go register notation

--- src/runtime/rt0_noos_mips64.s
+++ src/runtime/rt0_noos_mips64.s
@@ -67,118 +67,118 @@ loop:
 
 TEXT runtime·rt0_go(SB),NOSPLIT|NOFRAME|TOPFRAME,$0
 	// setup main stack in cpu0.gh
-	MOVV  $runtime·cpu0(SB), t0  // gh is the first field of the cpuctx struct
-	MOVV  $runtime·ramend(SB), sp  // main stack starts at the end of memory
-	SUB   $16, sp
-	MOVV  sp, (g_stack+stack_hi)(t0)
-	SUB   $4096, sp, t1
-	MOVV  t1, (g_stack+stack_lo)(t0)
-	ADD   $const_stackGuard, t1
-	MOVV  t1, g_stackguard0(t0)
-	MOVV  t1, g_stackguard1(t0)
+	MOVV  $runtime·cpu0(SB), R8  // gh is the first field of the cpuctx struct
+	MOVV  $runtime·ramend(SB), R29  // main stack starts at the end of memory
+	SUB   $16, R29
+	MOVV  R29, (g_stack+stack_hi)(R8)
+	SUB   $4096, R29, R9
+	MOVV  R9, (g_stack+stack_lo)(R8)
+	ADD   $const_stackGuard, R9
+	MOVV  R9, g_stackguard0(R8)
+	MOVV  R9, g_stackguard1(R8)
 
 	// set up m0 (bootstrap thread), temporarily use cpu0.gh as g
-	MOVV  $runtime·m0(SB), t1
-	MOVV  t0, m_g0(t1)  // m0.g0 = cpu0.gh
-	MOVV  t1, g_m(t0)   // cpu0.gh.m = m0
+	MOVV  $runtime·m0(SB), R9
+	MOVV  R8, m_g0(R9)  // m0.g0 = cpu0.gh
+	MOVV  R9, g_m(R8)   // cpu0.gh.m = m0
 
-	MOVV  t0, g  // set g to gh
+	MOVV  R8, g  // set g to gh
 
 	JAL runtime·check(SB)
 	JAL runtime·osinit(SB)
 
 	// initialize noosMem
 
-	MOVV  $runtime·end(SB), t0
-	MOVV  $runtime·ramend(SB), t1
-	SUB   $4096, t1
-	SUB   t0, t1, t5  // size of available memory (DMA capable)
+	MOVV  $runtime·end(SB), R8
+	MOVV  $runtime·ramend(SB), R9
+	SUB   $4096, R9
+	SUB   R8, R9, R13  // size of available memory (DMA capable)
 
 	// estimate the space need for non-heap allocations
-	SRL   $(const__PageShift), t5, t4
-	MOVV  $mspan__size, t2
-	MUL   t2, t4
-	MOVV  LO, t4
-	ADD   $PALLOC_MIN, t4
+	SRL   $(const__PageShift), R13, R12
+	MOVV  $mspan__size, R10
+	MUL   R10, R12
+	MOVV  LO, R12
+	ADD   $PALLOC_MIN, R12
 
-	MOVV  $runtime·nodmastart(SB), t2
-	MOVV  $runtime·nodmaend(SB), t3
-	SUB   t2, t3, t7  // size of non-DMA memory
-	ADD   t5, t7, t6  // size of the whole free memory
+	MOVV  $runtime·nodmastart(SB), R10
+	MOVV  $runtime·nodmaend(SB), R11
+	SUB   R10, R11, R15  // size of non-DMA memory
+	ADD   R13, R15, R14  // size of the whole free memory
 
 	// we prefer the non-DMA memory for non-heap objects to preserve as much as
 	// possible of the DMA capable memory for heap allocations
-	SUB   t7, t4
+	SUB   R15, R12
 
 	// reduce the arena by the remain of the non-heap space that did not fit in
 	// the non-DMA memory, properly align the arena
-	BLTZ  t4, 2(PC)
-	SUB   t4, t5
-	AND   $~(const_heapArenaBytes-1), t5
-	SUB   t5, t1
-	MOVV  t1, t4
+	BLTZ  R12, 2(PC)
+	SUB   R12, R13
+	AND   $~(const_heapArenaBytes-1), R13
+	SUB   R13, R9
+	MOVV  R9, R12
 
 	// save {free.start,free.end,nodma.start,nodma.end,arenaStart,arenaSize,size}
-	MOVV  $runtime·noosMem(SB), t7
-	MOVV  t0, 0(t7)
-	MOVV  t1, 8(t7)
-	MOVV  t2, 16(t7)
-	MOVV  t3, 24(t7)
-	MOVV  t4, 32(t7)
-	MOVV  t5, 40(t7)
-	MOVV  t6, 48(t7)
+	MOVV  $runtime·noosMem(SB), R15
+	MOVV  R8, 0(R15)
+	MOVV  R9, 8(R15)
+	MOVV  R10, 16(R15)
+	MOVV  R11, 24(R15)
+	MOVV  R12, 32(R15)
+	MOVV  R13, 40(R15)
+	MOVV  R14, 48(R15)
 
 	// initialize noos tasker and Go scheduler
 	JAL  runtime·taskerinit(SB)
 	JAL  runtime·schedinit(SB)
 
 	// allocate g0 for m0 and leave gh
-	SUB   $16, sp
-	MOVW  $(2*const_stackMin), a0
-	MOVW  a0, 8(sp)
+	SUB   $16, R29
+	MOVW  $(2*const_stackMin), R4
+	MOVW  R4, 8(R29)
 	JAL   runtime·malg(SB)
-	MOVV  16(sp), t0  // newg in t0
-	ADD   $16, sp
+	MOVV  16(R29), R8  // newg in R8
+	ADD   $16, R29
 
 	// stackguard check during newproc requires valid stackguard1 but
 	// malg sets it to 0xFFFFFFFF (mstart fixes this but is called later)
-	MOVV  g_stackguard0(t0), t1
-	MOVV  t1, g_stackguard1(t0)
+	MOVV  g_stackguard0(R8), R9
+	MOVV  R9, g_stackguard1(R8)
 
-	MOVV  $runtime·m0(SB), t1
-	MOVV  t0, m_g0(t1)  // m0.g0 = newg
-	MOVV  t1, g_m(t0)   // newg.m = m0
+	MOVV  $runtime·m0(SB), R9
+	MOVV  R8, m_g0(R9)  // m0.g0 = newg
+	MOVV  R9, g_m(R8)   // newg.m = m0
 
-	// newg stack pointer to sp
-	MOVV  (g_stack+stack_hi)(t0), sp
+	// newg stack pointer to R29
+	MOVV  (g_stack+stack_hi)(R8), R29
 
 	// newg to g
-	MOVV  g, t2
-	MOVV  t0, g
+	MOVV  g, R10
+	MOVV  R8, g
 
 	// fix cpu0.gh, cpu0.mh
-	ADD   $cpuctx_mh, t2, t1  // t2 points to cpu0 (and to cpu0.gh at the same time)
-	MOVV  t2, m_g0(t1)        // cpu0.mh.g0 = cpu0.gh
-	MOVV  t2, m_gsignal(t1)   // cpu0.mh.gsignal = cpu0.gh (to easily check for handler mode)
-	MOVV  t1, g_m(t2)         // cpu0.gh.m = cpu0.mh
+	ADD   $cpuctx_mh, R10, R9  // R10 points to cpu0 (and to cpu0.gh at the same time)
+	MOVV  R10, m_g0(R9)        // cpu0.mh.g0 = cpu0.gh
+	MOVV  R10, m_gsignal(R9)   // cpu0.mh.gsignal = cpu0.gh (to easily check for handler mode)
+	MOVV  R9, g_m(R10)         // cpu0.gh.m = cpu0.mh
 
 	// TODO switch to the user mode?
 
 	// create a new goroutine to start program
-	SUB	$16, sp
-	MOVV	$runtime·mainPC(SB), t0
-	MOVV	t0, 8(sp)	// arg 1: fn
-	MOVV	$0, t0
-	MOVV	t0, 0(sp)	// dummy LR
+	SUB	$16, R29
+	MOVV	$runtime·mainPC(SB), R8
+	MOVV	R8, 8(R29)	// arg 1: fn
+	MOVV	$0, R8
+	MOVV	R8, 0(R29)	// dummy LR
 	JAL	runtime·newproc(SB)
-	ADD	$16, sp	// pop args and LR
+	ADD	$16, R29	// pop args and LR
 
 	// enable interrupts
 	// TODO where to enable interupts correctly?
-	MOVW z0, M(C0_COMPARE)
-	MOVW M(C0_SR), t0
-	OR  $(SR_IE|INTR_SW|INTR_EXT), t0
-	MOVW t0, M(C0_SR)
+	MOVW R0, M(C0_COMPARE)
+	MOVW M(C0_SR), R8
+	OR  $(SR_IE|INTR_SW|INTR_EXT), R8
+	MOVW R8, M(C0_SR)
 
 	// start this M
 	JAL  runtime·mstart(SB)
