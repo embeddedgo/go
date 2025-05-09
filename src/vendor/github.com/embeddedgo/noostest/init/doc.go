a074416489e4e43a671a0479f57a5240a34d4994
Michał Derkacz <michal@Lnet.pl>
2025-05-09 12:55:43 +0200

    [merge go1.24] vendor/github.com/embeddedgo/noostest/init: temporary fix
    to avoid build error on GOOS!=noos

---
+++ src/vendor/github.com/embeddedgo/noostest/init/doc.go
@@ -0,0 +1,6 @@
+// Copyright 2024 The Embedded Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package init when imported setups the semihosting test environment.
+package init
