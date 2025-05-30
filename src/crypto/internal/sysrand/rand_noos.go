7c64e1acd0a268728ce9513c1ede3ee0a56d34c8
Michał Derkacz <michal@Lnet.pl>
2025-05-30 18:44:24 +0200

    [merge go1.24] crypto/internal/sysrand: stub implementation of read on noos

---
+++ src/crypto/internal/sysrand/rand_noos.go
@@ -0,0 +1,11 @@
+// Copyright 2010 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package sysrand
+
+import "syscall"
+
+func read(b []byte) error {
+	return syscall.ENOTSUP
+}
